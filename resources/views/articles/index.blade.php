01c62b5c2dcf20ddf57126a72e7576e6e8aefb70
Oleksandr Kovalchuk <sanyol255@gmail.com>
2023-05-01 12:57:36 +0300

    Displaying article author and date

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -9,13 +9,16 @@
     <main class="mt-6 ml-8 mr-6">
         <section>
             @foreach($articles as $article)
+                <hr class="mb-3">
                 <article class="mb-4">
                     <h2 class="text-center text-xl mb-2">
                         <a href="{{ route('articles.show', $article->id) }}" class="hover:text-teal-600">{{ $article->title }}</a>
                     </h2>
                     <p class="indent-12">{{ $article->body}}</p>
+                    <span class="float-left">Author: {{ $article->author->firstname }} {{ $article->author->lastname }}</span><span class="float-right">Date: {{ $article->created_at->format('M d, Y H:i') }}</span>
                 </article>
-
+                <br>
+                <hr>
             @endforeach
         </section>
     </main>
